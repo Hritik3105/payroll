c00bbfca5ba2e428d6b72d1cd0db5a144ff0356f
webdeveloper510 <webdeveloper510@gmail.com>
2023-01-06 17:39:51 +0530

    future

--- payrollapp/views/PaidView.py
+++ payrollapp/views/PaidView.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from payrollapp.models import *
 
 
 @login_required 
 def paid(request):
-    print("enterrrsdfsdf")
+    obj_pro=Providers.objects.filter(user_id=request.user.id)
    
-    return render(request,"paid/paid.html")
+    return render(request,"paid/paid.html",{"obj":obj_pro})
+
+
+
+
+#from where we will know balance is paid or not ??
