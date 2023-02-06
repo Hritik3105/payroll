789e634c14456b2bd4496621eae738d8b684d1c0
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-06 17:12:45 +0530

    bold

--- payroll/settings.py
+++ payroll/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = 'django-insecure-is@ig0!b_95wgv1$fw_x(eo^rb5#nnox_z8))*(t8t-^cg(4rf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =[]
 
-ALLOWED_HOSTS =['52.90.37.50']
+# ALLOWED_HOSTS =['52.90.37.50']
 
 
 # Application definition
@@ -90,30 +90,30 @@ AUTH_USER_MODEL = 'payrollapp.user'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'payroll',
-#         'USER':'root',
-#         "HOST":"localhost",
-#         'PASSWORD':"",
-#         "PORT":"3306"
-#     }
-# }
-
-
-# git
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'payroll',
         'USER':'root',
         "HOST":"localhost",
-        'PASSWORD':"AdMiN@1234",
+        'PASSWORD':"",
         "PORT":"3306"
     }
 }
+
+
+# git
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'payroll',
+#         'USER':'root',
+#         "HOST":"localhost",
+#         'PASSWORD':"AdMiN@1234",
+#         "PORT":"3306"
+#     }
+# }
 
 USE_THOUSAND_SEPARATOR = True
 
