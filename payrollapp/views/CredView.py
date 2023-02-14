d8dcf327d95a0282a33fcd31d1038117c2d1f422
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-14 13:12:21 +0530

    345

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -62,7 +62,7 @@ def sii(request,siiusernae,password,month,year):
     
   
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless') 
+    options.add_argument('--headless=chrome')
     # prefs = {"download.default_directory" : z}
     # options.add_experimental_option("prefs",prefs)
                                     
