dee89d2ee50341eb7a1a0a94f6052ea8d8550ed9
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-15 16:11:23 +0530

    change

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -122,7 +122,7 @@ def sii(request,siiusernae,password,month,year):
 
     driver.find_element(By.XPATH,"//button[text()='Descargar Detalles']").click()
     time.sleep(3)
-    shutil.copyfile("/home/ubuntu/payroll/payrollapp/csv", "/home/ubuntu/payroll/csv")
+    shutil.copyfile("/home/nirmla/Desktop/payroll/payrollapp/csv1", "/home/nirmla/Desktop/payroll/csv1")
     messages.success(request,"CSV Downloaded Successfull",extra_tags="company")
 
   except Exception as e:
