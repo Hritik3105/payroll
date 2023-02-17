01805eb8ef6cf0e7d76f6fa9be785b6d83c972ce
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-17 15:37:57 +0530

    changes

--- payrollapp/views/DueView.py
+++ payrollapp/views/DueView.py
@@ -43,7 +43,7 @@ def due_table(request):
         
 
 
-    print(datetime.date.today())
+    # print(datetime.date.today())
     all_obj=Providers.objects.filter(user_id=request.user.id)
     len_obj=len(all_obj) 
     if request.method =="POST" and  'due' in request.POST:
