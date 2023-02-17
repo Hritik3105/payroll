644d9b990763e7b3624e744b8d13166172d698cf
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-17 16:40:18 +0530

    changes

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -70,8 +70,9 @@ def sii(request,siiusernae,password,month,year):
 
 
     options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    
+    
 
     
     # prefs = {"download.default_directory" : z}
