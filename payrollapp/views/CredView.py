1657c38f9049ada48b0ecee31d1ffc8070a6578e
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-23 12:39:19 +0530

    123

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -109,9 +109,11 @@ def sii(request,siiusernae,password,month,year):
 
     driver.find_element(By.XPATH,"//button[text()='Descargar Detalles']").click()
     time.sleep(3)
+    download_location = driver.execute_script('return window.navigator.userAgent')
+    filename = 'new_filename.csv'   
+    save_button.send_keys("/home/ubuntu/Downloads" + '\\' + filename)
 
-    downloaded_file_name = "filename.csv"
-    downloaded_file_path = os.path.join("/home/ubuntu/Downloads/", downloaded_file_name)
+    save_button = driver.find_element_by_xpath('//button[@id="save-button"]')
     
     messages.success(request,"CSV Downloaded Successfull",extra_tags="company")
     time.sleep(10)
