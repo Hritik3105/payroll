8113f2787a17e032d8e64f4767952b4566ce2e88
webdeveloper510 <webdeveloper510@gmail.com>
2023-02-24 15:57:13 +0530

    changes

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -74,19 +74,9 @@ def sii(request,siiusernae,password,month,year):
     
 
 
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')  
-  
-    options.add_argument("--no-sandbox") 
- 
-    # options.add_experimental_option("prefs", {
-    # "download.default_directory": download_dir,
-    #  "download.prompt_for_download": False,
+    options.add_argument('--headless=chrome')
     
-# })
 
-
-                                  
     
     serv_obj = Service()
     driver = webdriver.Chrome(options=options,service = serv_obj)
