29af4fadb9d094d86df70463ac43abd4e1c6ca61
webdeveloper510 <webdeveloper510@gmail.com>
2023-03-06 15:28:13 +0530

    345

--- payrollapp/views/CredView.py
+++ payrollapp/views/CredView.py
@@ -296,11 +296,12 @@ def sii(request,siiusernae,password,month,year):
 
       files_download = os.listdir("/home/ubuntu/Downloads")
       print("--------------------",files_download)
-      for i in files_download:
-        print("enterrr33",i)
-        if "(1)" not  in i: 
-          print(i)
-          os.remove("/home/ubuntu/Downloads/" + i)
+      if len(files_download) == 0:
+        for i in files_download:
+          print("enterrr33",i)
+          if "(1)" not  in i: 
+            print(i)
+            os.remove("/home/ubuntu/Downloads/" + i)
         
                 
       file_exists = exists("/home/ubuntu/payroll/payrollapp/"+request.user.username)
